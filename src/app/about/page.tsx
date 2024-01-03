87be2f985523851bb30c43c17ea14faf479f1b93
david-mendes-s <david.a.mendes.s@gmail.com>
2024-01-03 10:42:21 -0300

    mudanças no about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -28,15 +28,7 @@ export default function About(){
                     <h1 className='text-xl'>SOBRE MIM</h1>
                     <p className="text-sm text-[#797979] mt-4">
                     It is a long established fact that a reader will be distracted 
-                    by the readable content of a page when looking at its layout. 
-                    The point of using Lorem Ipsum is that it has a more-or-less normal 
-                    distribution of letters, as opposed to using 
-                    Content here, content here, making it look like readable English.
-                    Many desktop publishing packages and web page editors now use 
-                    Lorem Ipsum as their default model text, and a search for 
-                    lorem ipsum will uncover many web sites still in their infancy. 
-                    Various versions have evolved over the years, sometimes by accident, 
-                    sometimes on purpose injected humour and the like.
+                    by the readable content of a page when looking.
                     </p>
 
                     <p className="text-sm text-[#797979] mt-4">
