87f10da90246292b7a91b87a5d266a59876e6d67
david-mendes-s <david.a.mendes.s@gmail.com>
2024-01-03 11:03:25 -0300

    animation create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
       <main className="">
         <div className="flex max-md:flex-wrap  gap-6 mt-10">
-          <div className="flex max-sm:flex-col max-sm:justify-center max-sm:items-center gap-6 w-1/2 bg-[#1B1B1B] max-md:w-full p-6 rounded-2xl">
+          <div className="flex max-sm:flex-col max-sm:justify-center max-sm:items-center gap-6 w-1/2 bg-[#1B1B1B] max-md:w-full p-6 rounded-2xl scale-up-center">
             <div className="flex items-end justify-start  w-1/2 max-sm:h-52 bg-[#7B9CF0] rounded-br-3xl rounded-tl-3xl">
               {/* <Image src={Avatar} alt='logo' className='w-72 h-auto rounded-br-3xl'/> */}
             </div>
@@ -25,14 +25,14 @@ export default function Home() {
             </div>           
           </div>
           <div className="w-1/2  max-md:w-full rounded-2xl">
-            <div className="bg-[#1B1B1B] py-2 px-4 rounded-2xl">
+            <div className="bg-[#1B1B1B] py-2 px-4 rounded-2xl scale-up-center">
               <ul className='flex flex-nowrap text-xs text-[#797979] font-bold'>
                 <li className='text-white'>HABILIDADES EM: <p className='text-[#797979] inline'>HTML - CSS - JS - ReactJS</p> </li>
                 
               </ul>
             </div>
             <div className="flex items-center justify-center gap-6 mt-4">
-              <div className="flex  flex-col justify-between gap-5 w-1/2 h-52 bg-[#1B1B1B] p-4 rounded-2xl">
+              <div className="flex  flex-col justify-between gap-5 w-1/2 h-52 bg-[#1B1B1B] p-4 rounded-2xl scale-up-center">
                     <div className='flex items-end max-lg:flex-shrink-0 w-20 h-20  rounded-2xl'>
                       <BsBuildingCheck size={35} color='#fff'/>
                     </div>
@@ -50,7 +50,7 @@ export default function Home() {
                     </div>
               </div>
 
-              <div className="flex  flex-col justify-between gap-5 w-1/2 h-52  bg-[#1B1B1B] p-4 rounded-2xl">
+              <div className="flex  flex-col justify-between gap-5 w-1/2 h-52  bg-[#1B1B1B] p-4 rounded-2xl scale-up-center">
                     <div className='flex items-end w-20 h-20 max-lg:flex-shrink-0  rounded-2xl'>
                     
                       <BsColumns size={35} color='#fff'/>
@@ -74,9 +74,9 @@ export default function Home() {
         </div>
 
         <div className="flex max-md:flex-wrap gap-6 mt-8">
-            <div className="flex w-2/5 gap-6 max-md:w-full max-md:order-2">
+          <div className="flex w-2/5 gap-6 max-md:w-full max-md:order-2">
               
-            <div className="flex flex-col  gap-5 w-1/2 h-52 bg-[#1B1B1B] p-4 rounded-2xl">
+            <div className="flex flex-col  gap-5 w-1/2 h-52 bg-[#1B1B1B] p-4 rounded-2xl scale-up-center">
                     <div className='flex items-end w-20 h-20 max-sm:flex-shrink-0 rounded-2xl'>
                       <BsInstagram size={35} color='#fff'/>
                     
@@ -95,7 +95,7 @@ export default function Home() {
                     </div>
               </div>
 
-              <div className="flex flex-col gap-5 w-1/2 h-52 bg-[#1B1B1B] p-4 rounded-2xl">
+              <div className="flex flex-col gap-5 w-1/2 h-52 bg-[#1B1B1B] p-4 rounded-2xl scale-up-center">
                     <div className='flex items-end w-20 h-20 max-sm:flex-shrink-0 rounded-2xl'>
                       <BsJournalCheck size={35} color='#fff'/>
                     
@@ -116,7 +116,7 @@ export default function Home() {
             </div>
               
 
-            <div className="w-3/5 max-md:w-full max-md:order-1 bg-[#1B1B1B] p-4 rounded-2xl">
+            <div className="w-3/5 max-md:w-full max-md:order-1 bg-[#1B1B1B] p-4 rounded-2xl scale-up-center">
               <div className='w-ful'>
                 <ul className='flex items-center  w-full justify-center gap-6 overflow-x-hidden'>
                     <li className='flex items-end w-20 h-20  rounded-2xl'>
