498cce36e7199ced3b4bbddde2ba2fb7b28977f4
david-mendes-s <david.a.mendes.s@gmail.com>
2024-01-05 10:32:03 -0300

    animetion scrool infinit

--- src/components/Carrossel.tsx
+++ src/components/Carrossel.tsx
@@ -15,24 +15,25 @@ export default function Carrossel({elements, width}: ICarrossel) {
   const scrollers = useRef<ScrollerElement[]>([]); // Cria um array de refs
   /* const scroller = document.querySelectorAll(".scroller"); */
 
-  useEffect(() => {
+ /*  useEffect(() => {
     if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
       addAnimation(); // Chama a função após os elementos serem montados
     }
-  }, []); // Roda o efeito apenas uma vez na montagem
+  }, []); */ // Roda o efeito apenas uma vez na montagem
 
-  function addAnimation() {
+  /* function addAnimation() {
     scrollers.current?.forEach((element) => {
       if (element) {
         element.setAttribute("data-animated", "true")
       }
     });
-  }
+  } */
 
   return (
     <>
       {/* Associa as refs aos elementos */}
       <div
+        data-animated="true"
         className={`max-w-[${width}px] scroller px-2`}
         ref={(el) => (scrollers.current[0] = el as ScrollerElement)}
       >
